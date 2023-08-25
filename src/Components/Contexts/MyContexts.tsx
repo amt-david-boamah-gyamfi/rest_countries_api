a06fc2ff25d1c51d889fcf3ee15890baea19ac54
David Boamah Gyamfi <123944118+dbgyamfi@users.noreply.github.com>
2023-08-25 04:23:44 +0000

    Update MyContexts.tsx

--- src/Components/Contexts/MyContexts.tsx
+++ src/Components/Contexts/MyContexts.tsx
@@ -1,16 +1,7 @@
 import { createContext } from 'react';
 import { CountriesTs } from '../Types/MyTypes';
 
-export const themeSetting = {
-    light: {
-        text: 'hsl(200, 15%, 8%)',
-        background: 'hsl(0, 0%, 98%)',
-    },
-    dark: {
-        text: 'hsl(0, 0%, 100%)',
-        background: 'hsl(207, 26%, 17%)',
-    },
-};
+
 
 
 interface ThemeAndApi {
